0cf9c0bfe0d4d60f3b817bd419fc50031318dc05
Joe Kollie <joekollie020@gmail.com>
2024-10-13 13:40:51 -0500

    Added lines of code to enhance the application

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ if(process.env.NODE_ENV!== "production") {
     require('dotenv').config();
 }
 
+// require('dotenv').config();
+
+
+
 console.log(process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_KEY, process.env.CLOUD_SECRET);
 
 
@@ -10,6 +14,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 const flash = require('connect-flash');
 const {campgroundSchema, reviewSchema} = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
@@ -18,13 +24,17 @@ const passport = require('passport');
 const { captureRejectionSymbol } = require('events');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/user.js')
 const campgroundRoutes = require('./routes/campgrounds.js');
 const reviewRoutes = require('./routes/reviews.js')
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
+// 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
      useNewUrlParser: true,
      //useCreateIndex: true,
     useUnifiedTopology: true
@@ -45,19 +55,84 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true }))
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')))
+app.use(mongoSanitize());
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    }
+});
+
+store.on("error", function(e){
+    console.log("SESSION STORE ERROR",e)
+})
 
 const sessionConfig = {
+    store,
+    name: 'session',
     secret: 'thisshouldbeabettersecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true, //set that to true when deploying the app
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
+
+
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudflare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com/",
+//     "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/dnru4lopa/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//                 "https://images.unsplash.com/",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -67,6 +142,7 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
--- schemas.js
+++ schemas.js
@@ -1,12 +1,50 @@
-const Joi = require('joi')
+const Joi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+
+//Define a custom Joi validation for HTML escape/sanitize
+
+const escapeHTML = (value, helpers) => {
+    const clean = sanitizeHtml(value, {
+        allowedTags: [], //Remove all HTML tags
+        allowedAttributes: {}, //Remove all attributes
+    });
+    if (clean !== value) {
+        return helpers.error('string.escapeHTML', { value });
+    }
+    return clean; // Return the sanitized string
+}
+
+// const extension = (joi) => ({
+//     type: 'string',
+//     base: joi.string(),
+//     messages: {
+//         'string.escapeHTML': '{{#label}} must not include HTML!'
+//     },
+//     rules: {
+//         escapeHTML: {
+//             Validate(value, helpers) {
+//                 const clean = sanitizeHtml(value, {
+//                     allowedTags: [],
+//                     allowedAttributes: {},
+//                 });
+//                 if (clean !== value) return helpers.error('string.escapeHTML', {value})
+//                 return clean;
+//             }
+//         }
+//     }
+
+// });
+
+// const Joi = Joi.extend(extension)
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string(),
+        title: Joi.string().custom(escapeHTML).message({'string.escapeHTML': 'Title must not include HTML tags!'}),
         price: Joi.number().min(0),
         // image: Joi.string().required(),
-        location: Joi.string(),
-        description: Joi.string().required()
+        location: Joi.string().custom(escapeHTML).message({'string.escapeHTML': 'Title must not include HTML tags!'}),
+        description: Joi.string().required().custom(escapeHTML).message({'string.escapeHTML': 'Title must not include HTML tags!'})
     }).required(),
     deleteImages:Joi.array()
 
@@ -16,6 +54,8 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        body: Joi.string().required().custom(escapeHTML).message({'string.escapeHTML': 'Title must not include HTML tags!'})
     }).required()
 })
+
+
